8ca49450837d1653c0fbd81eb215c4d28331967c
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-11-01 18:02:00 -0300

    add

---
+++ main.js
@@ -0,0 +1,12 @@
+const icon = document.getElementById('menu');
+let nav = document.getElementById ('nav');
+
+icon.addEventListener('click', () => {
+    if(nav.style.left === '0rem') {
+        nav.style.left = '-21rem';
+    }
+    else {
+        nav.style.left = '0rem';
+    }
+})
+
